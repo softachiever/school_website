269af3819794be5faf5ccd4ee860ba7c72e3e257
nwachukwu prince <coderight12@gmail.com>
2022-12-08 18:11:41 +0100

    category update

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
     ('window-scroll',window.scrollY > 100)
-})
+});
+
+/*changing and hidding nav styles*/
+const menu =document.querySelector(".nav__menu");
+
+const open = document.querySelector(".open-menu-btn");
+
+const close = document.querySelector(".close-menu-btn");
+
+function open (){
+    
+    menu.style.display = "block";
+    open.style.display = "inline-block";
+    close.style.display = "none"
+
+}
+
+open.addEventListener("click", open);
+
